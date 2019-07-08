29d5ac0c6ea6ec4908b4f49f9f45cf01a6bcd927
dymasainun <nundymas@gmail.com>
2019-07-08 23:56:57 +0700

    product finish

--- app/src/main/java/com/nun/codeonline/Controller/ProductsActivity.kt
+++ app/src/main/java/com/nun/codeonline/Controller/ProductsActivity.kt
@@ -1,11 +1,14 @@
 package com.nun.codeonline.Controller
 
+import android.content.res.Configuration
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
 import com.nun.codeonline.Adapters.ProductsAdapter
 import com.nun.codeonline.R
 import com.nun.codeonline.Services.DataService
 import com.nun.codeonline.Utilities.EXTRA_CATEGORY
+import kotlinx.android.synthetic.main.activity_products.*
 
 class ProductsActivity : AppCompatActivity() {
     lateinit var adapter : ProductsAdapter
@@ -16,5 +19,19 @@ class ProductsActivity : AppCompatActivity() {
 
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
         adapter = ProductsAdapter(this, DataService.getProduct(categoryType))
+
+        var spanCount = 2
+        val orientation = resources.configuration.orientation
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            spanCount = 4
+        }
+        val screenSize = resources.configuration.screenWidthDp
+        if (screenSize > 720) {
+            spanCount = 3
+        }
+
+        val layoutManager = GridLayoutManager(this,spanCount)
+        productsListView.layoutManager = layoutManager
+        productsListView.adapter = adapter
     }
 }
